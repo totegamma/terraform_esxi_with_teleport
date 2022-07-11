71d5fa13ec5ae7df5edb1596aa398f31b52cacce
thotgamma <github@gammalab.net>
2022-07-11 23:14:46 +0900

    add node

--- main.tf
+++ main.tf
@@ -25,6 +25,13 @@ resource "esxi_virtual_disk" "proxy_disk" {
 	virtual_disk_size       = 32
 }
 
+resource "esxi_virtual_disk" "node_disk" {
+	virtual_disk_disk_store = "Data"
+	virtual_disk_dir        = "Terraform"
+	virtual_disk_name       = "node_disk.vdmk"
+	virtual_disk_size       = 32
+}
+
 resource "esxi_guest" "teleport_proxy" {
 
 	guest_name     = "teleport_proxy"
@@ -52,23 +59,32 @@ resource "esxi_guest" "teleport_proxy" {
 	}
 }
 
-# resource "esxi_guest" "teleport_node" {
-# 
-# 	guest_name     = "teleport_node"
-# 	disk_store     = "Data"
-# 	virthwver      = "11"
-# 	power          = "on"
-# 	boot_disk_size = 32
-# 	guestos        = "ubuntu"
-# 	ovf_source     = "./bionic-server-cloudimg-amd64.ova"
-# 
-# 	network_interfaces {
-# 		virtual_network = "playground"
-# 	}
-# 	guestinfo = {
-# 		"userdata"          = base64gzip(file("./node/cloud-init.yaml"))
-# 		"userdata.encoding" = "gzip+base64"
-# 	}
-# }
+resource "esxi_guest" "teleport_node" {
+
+	guest_name     = "teleport_node"
+	disk_store     = "Data"
+	virthwver      = "11"
+	power          = "on"
+	boot_disk_size = 32
+	guestos        = "ubuntu"
+	ovf_source     = "./bionic-server-cloudimg-amd64.ova"
+
+	network_interfaces {
+		virtual_network = "playground"
+	}
+
+	virtual_disks {
+		virtual_disk_id = esxi_virtual_disk.node_disk.id
+		slot            = "0:1"
+	}
+
+	guestinfo = {
+		"userdata"          = base64gzip(file("./node/cloud-init.yaml"))
+		"userdata.encoding" = "gzip+base64"
+		"metadata"          = base64gzip(file("./node/meta.yaml"))
+		"metadata.encoding" = "gzip+base64"
+	}
+
+}
 
 
